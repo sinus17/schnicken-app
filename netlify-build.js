d78d8197628846a22dd22fa31b650b2eb545f1b3
Philipp Lützenburger <philipp@luetzenburger.org>
2025-07-08 19:05:50 +0200

    Fix build script: Convert from CommonJS to ES modules

--- netlify-build.js
+++ netlify-build.js
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
-const { execSync } = require('child_process');
-const fs = require('fs');
-const path = require('path');
+import { execSync } from 'child_process';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Get directory name in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 console.log('🚀 Starting custom Netlify build script');
 
